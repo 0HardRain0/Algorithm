2e3c8adcd4b16b0dac24ddf17fb27f8991c7c863
Wooyeol Baek <45348509+0HardRain0@users.noreply.github.com>
2022-11-09 00:11:57 +0900

    Add files via upload

---
+++ 221108/Tabular_system(2).cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+#define KETA 20
+#define N ((KETA-1)/4+1)
+
+void lmul(short *, short, short *);
+void ldiv(short *, short, short *);
+void print(short*);
+
+void main()
+{
+	short a[N + 2] = { 0,3050,2508,8080,1233 },
+		  c[N + 2];
+
+	lmul(a, 101, c); print(c);
+	ldiv(a, 200, c); print(c);
+}
+
+void lmul(short a[], short b, short c[])
+{
+	short i; long d, cy = 0;
+	for (i = N - 1; i >= 0; i--) {
+		d = a[i];
+		c[i] = (d * b + cy) % 10000;
+		cy = (d * b + cy) / 10000;
+	}
+}
+
+void ldiv(short a[], short b, short c[])
+{
+	short i; long d, rem = 0;
+	for (i = 0; i < N; i++)
+	{
+		d = a[i];
+		c[i] = (short)((d + rem) / b);
+		rem = ((d + rem) % b) * 10000;
+	}
+}
+
+void print(short c[])
+{
+	short i;
+	for (i = 0; i < N; i++)
+		printf("%04d ", c[i]);
+	printf("\n");
+}
