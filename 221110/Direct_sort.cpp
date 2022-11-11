d427950d59ade595e47fd70532da48a451be2fa7
Wooyeol Baek <45348509+0HardRain0@users.noreply.github.com>
2022-11-11 23:05:40 +0900

    Add files via upload

---
+++ 221110/Direct_sort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+#define N 6
+
+void main()
+{
+	int a[] = { 80,41,35,90,40,20 };
+	int min, s, t, i, j;
+
+	for (i = 0; i < N - 1; i++) {
+		min = a[i];
+		s = i;
+		for (j = i + 1; j < N; j++) {
+			if (a[j] < min) {
+				min = a[j];
+				s = j;
+			}
+		}
+		t = a[i]; a[i] = a[s]; a[s] = t;
+	}
+
+	for (i = 0; i < N; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+
+}
