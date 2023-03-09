29c31313e1db5c1e976d41e21cb5c013bd24e6d7
Wooyeol Baek <45348509+0HardRain0@users.noreply.github.com>
2023-03-10 00:00:29 +0900

    Create BinarySearchTree.cpp

---
+++ 230309/BinarySearchTree.cpp
@@ -0,0 +1,48 @@
+// ���� �޸𸮻󿡼��� Ʈ�� ǥ��
+#include<iostream>
+#include<string.h>
+
+
+//����ü
+struct tnode {
+	struct tnode* left;
+	char name[12];
+	struct tnode* right;
+} dat[12];
+
+struct tnode* talloc(void);
+
+int main(void) {
+
+	char dat[12];
+	struct tnode* root, * p, * old = {0};
+
+	root = talloc();
+	scanf_s("%s", root->name, 80);
+	root->left = root->right = NULL;
+
+	while (scanf_s("%s", dat, 80) != EOF) {
+		p = root;
+		while (p != NULL) {
+			old = p;
+			// 
+			if (strcmp(dat, p->name) <= 0)
+				p = p-> left;
+			else
+				p = p-> right;
+		}
+
+		p = talloc();
+		strcpy_s(p->name, 12, dat);
+		p->left = p->right = NULL;
+		if (strcmp(dat, old->name) <= 0)
+			old->left = p;
+		else
+			old->right = p;
+	}
+}
+
+struct tnode* talloc(void)
+{
+	return (struct tnode*)malloc(sizeof(struct tnode));
+}
