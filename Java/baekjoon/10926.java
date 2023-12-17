1053be297b1bcdf75b3993184fc9f5b1618b540e
Wooyeol Baek <45348509+0HardRain0@users.noreply.github.com>
2023-12-17 23:24:30 +0900

    Add files via upload

---
+++ Java/baekjoon/10926.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+// 스캐너 써도 됨 근데 스캐너 쪽이 느림
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        System.out.println(str + "??!");
+
+
+    }
+}
