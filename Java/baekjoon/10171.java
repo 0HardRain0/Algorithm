01b6692dd099cb3edae79d5cdab0a30dbf3e3dee
Wooyeol Baek <45348509+0HardRain0@users.noreply.github.com>
2024-01-24 22:44:42 +0900

    Add files via upload

---
+++ Java/baekjoon/10171.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        BigInteger result = new BigInteger("0");
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
